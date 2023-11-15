cdbd7f0be5e4e7286818cf546fcb23a9a133006a
Ezequiel-Cheque <delaluzezequiel@gmail.com>
2023-11-15 00:26:48 -0600

    FEAT: finish about page

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -1,8 +1,9 @@
+import "../style/camp.css";
 
 export const Camp = () => {
     return(
-        <>
+        <div className="camp-container">
             <h1>Camp</h1>
-        </>
+        </div>
     )
 };
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -10,24 +10,11 @@ export const Info = () => {
                 <div className="info-text">
                     <p>
                         <h3>Misión</h3>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus tenetur delectus neque omnis provident hic quis ea! Vero adipisci reiciendis aliquam iure molestias enim? Tempore iusto nemo repellendus error neque.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
+                        Servir al Señor con nuestros dones y talentos, brindando herramientas que busquen que cada jóven de la convención regional Bautista Elim, pueda tener un encuentro personal con el Señor y una vida devocional constante.
                     </p>
                     <p>
                         <h3>Visión</h3>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias officiis eveniet sapiente! Nobis ipsam quod, laborum aliquid, sapiente reiciendis assumenda ratione animi facere, eaque perferendis? Aliquam officia odit iste perspiciatis.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, sequi pariatur? Pariatur illum delectus mollitia animi dicta recusandae molestiae voluptatum totam expedita excepturi! Nobis, esse. Fuga adipisci vero iure dolorem.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatum corporis odio fugit asperiores aspernatur sapiente, vero atque recusandae neque quas, sequi nesciunt maxime doloribus saepe velit obcaecati debitis tempora.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam fugiat recusandae nostrum rerum exercitationem, consequatur deleniti ullam dolorum maxime vel suscipit nulla reprehenderit ab delectus placeat soluta amet at nihil.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus veniam inventore ex, voluptatibus cumque suscipit! Natus est exercitationem in nesciunt voluptates maxime! Odio praesentium impedit eligendi tempora consequuntur mollitia sequi.
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam quam non cum expedita excepturi accusamus aliquid quia vel sit repellendus voluptas cumque ex fugiat illum, pariatur consequatur qui? Facilis, fugit?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio porro ipsam dolores iure possimus, provident asperiores, iusto accusamus nihil nemo cupiditate esse explicabo earum tenetur et doloremque dolore voluptatum velit.
+                        Ser una federación que se distinga por jóvenes que amen al Señor, sirvan en sus iglesias locales y tengan un compañerismo mutuo.
                     </p>
                 </div>
             </div>
