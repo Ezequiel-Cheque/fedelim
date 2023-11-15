e90df76c2a1df3f5b7a124c3347c15f4e9fcde23
Ezequiel-Cheque <delaluzezequiel@gmail.com>
2023-11-14 23:13:50 -0600

    FIX: fix responsive

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,8 +1,36 @@
+import "../style/info.css";
 
 export const Info = () => {
     return(
-        <>
-            <h1>Info</h1>
-        </>
+        <div className="info-container">
+            <div className="div-info">
+                <div className="logo-info">
+                    <img src="src/assets/elim_square.png" alt="" />
+                </div>
+                <div className="info-text">
+                    <p>
+                        <h3>Misión</h3>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus tenetur delectus neque omnis provident hic quis ea! Vero adipisci reiciendis aliquam iure molestias enim? Tempore iusto nemo repellendus error neque.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, impedit! Nostrum doloribus laudantium, quo libero repellendus sapiente aliquam, ut porro neque qui sed fugit officiis non similique eum accusamus ex.
+                    </p>
+                    <p>
+                        <h3>Visión</h3>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias officiis eveniet sapiente! Nobis ipsam quod, laborum aliquid, sapiente reiciendis assumenda ratione animi facere, eaque perferendis? Aliquam officia odit iste perspiciatis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, sequi pariatur? Pariatur illum delectus mollitia animi dicta recusandae molestiae voluptatum totam expedita excepturi! Nobis, esse. Fuga adipisci vero iure dolorem.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatum corporis odio fugit asperiores aspernatur sapiente, vero atque recusandae neque quas, sequi nesciunt maxime doloribus saepe velit obcaecati debitis tempora.
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam fugiat recusandae nostrum rerum exercitationem, consequatur deleniti ullam dolorum maxime vel suscipit nulla reprehenderit ab delectus placeat soluta amet at nihil.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus veniam inventore ex, voluptatibus cumque suscipit! Natus est exercitationem in nesciunt voluptates maxime! Odio praesentium impedit eligendi tempora consequuntur mollitia sequi.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam quam non cum expedita excepturi accusamus aliquid quia vel sit repellendus voluptas cumque ex fugiat illum, pariatur consequatur qui? Facilis, fugit?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio porro ipsam dolores iure possimus, provident asperiores, iusto accusamus nihil nemo cupiditate esse explicabo earum tenetur et doloremque dolore voluptatum velit.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 };
