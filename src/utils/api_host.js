98857f071abe67354279f36a129afaeccf041aca
Ezequiel-Cheque <delaluzezequiel@gmail.com>
2023-11-17 14:36:17 -0600

    ngrok

--- src/utils/api_host.js
+++ src/utils/api_host.js
@@ -1,5 +1,8 @@
 
-const base_api = "http://ec2-54-86-233-209.compute-1.amazonaws.com:3000";
 // const base_api = "http://localhost:3000";
+
+// const base_api = "http://ec2-54-86-233-209.compute-1.amazonaws.com:3000";
+
+const base_api = "https://4c27-54-86-233-209.ngrok-free.app"
 
 export default base_api;
